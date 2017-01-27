05c62885e216844ffb1a1af9f219ab5878f384d7
Alex <agardner329@gmail.com>
2017-01-27 16:50:40 -0500

    Initial Commit

---
+++ src/com/Gerrick/DiscordBot.java
@@ -0,0 +1,30 @@
+package com.Gerrick;
+
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+
+/**
+ * Created by Alex Gardner on 1/27/2017.
+ */
+public class DiscordBot {
+
+    private static final String TOKEN = "274642025465511936";
+
+    private static JDA jda;
+
+    public static void Main(String[] args){
+
+        try{
+
+            jda = new JDABuilder(AccountType.BOT).setToken(TOKEN).buildBlocking();
+
+        } catch(Exception e){
+
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
