9a6bb222847e214b4fd4c8adb8c3794105949776
Alex <agardner329@gmail.com>
2017-01-31 21:48:05 -0500

    add sendNewRoundMessage, sendResistanceIntro

--- src/main/java/game/GameMessages.java
+++ src/main/java/game/GameMessages.java
@@ -3,6 +3,9 @@ package main.java.game;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.User;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -18,19 +21,65 @@ class GameMessages {
     private static final File PASS = new File("resources/images/Pass.png");
     private static final File FAIL = new File("resources/images/Fail.png");
 
-    protected static void sendNewRoundMessage(TextChannel channel, User commander, int numMembers){
+    protected static void sendMissionResults(TextChannel channel, boolean[] results){
 
-        sendMessageToGame(channel, commander.getName() + " is now the commander.\nThis mission will have " + numMembers + "members.");
+        int numFails = 0;
+        int numSuccesses = 0;
+
+        for(boolean result : results){
+
+            if(result){
+
+                numSuccesses++;
+
+            }else{
+
+                numFails++;
+
+            }
+
+        }
+
+        BufferedImage resultImage = new BufferedImage(816 * results.length, 1110, BufferedImage.TYPE_INT_RGB);
+        Graphics g = resultImage.getGraphics();
+        int currentX = 0;
+
+        try {
+            for (int i = 0; i < numSuccesses; i++) {
+
+                g.drawImage(ImageIO.read(PASS), currentX, 0, null);
+                currentX += 816;
+
+            }
+
+            for (int i = 0; i < numFails; i++) {
+
+                g.drawImage(ImageIO.read(FAIL), currentX, 0, null);
+                currentX += 816;
+
+            }
+
+            File tempFile = new File("temp.png");
+
+            ImageIO.write(resultImage, "png", tempFile);
+
+            sendImageToGame(channel, tempFile);
+
+            tempFile.delete();
+
+        }catch (IOException e){
+
+            e.printStackTrace();
+
+
+
+        }
 
     }
 
-    protected static void sendMessageToPlayers(User[] players, String message){
+    protected static void sendNewRoundMessage(TextChannel channel, User commander, int numMembers){
 
-        for(User player : players){
-
-            sendMessageToPlayer(player, message);
-
-        }
+        sendMessageToGame(channel, commander.getName() + " is now the commander.\nThis mission will have " + numMembers + "members.");
 
     }
 
@@ -71,6 +120,18 @@ class GameMessages {
             }
 
             sendMessageToPlayer(u, message);
+
+        }
+
+    }
+
+    protected static void sendResistanceIntro(User[] resistance){
+
+        for(User u : resistance){
+
+            sendImageToPlayer(u, TOWN);
+
+            sendMessageToPlayer(u, "You are on the Resistance");
 
         }
 
